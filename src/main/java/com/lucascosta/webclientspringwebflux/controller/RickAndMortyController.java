54c359a7a0bca9c9048d3625fdd6ee904bbddfc0
0xaf0y <lucas.santoscosta@gmail.com>
2024-01-14 09:44:25 -0300

    Implementação do client e controller para ler uma uri externa.

---
+++ src/main/java/com/lucascosta/webclientspringwebflux/controller/RickAndMortyController.java
@@ -0,0 +1,23 @@
+package com.lucascosta.webclientspringwebflux.controller;
+
+import com.lucascosta.webclientspringwebflux.client.RickAndMortyClient;
+import com.lucascosta.webclientspringwebflux.response.CharacterResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/webclient")
+public class RickAndMortyController {
+
+    RickAndMortyClient rickAndMortyClient;
+
+    @GetMapping("/character/{id}")
+    public Mono<CharacterResponse> getCharacterById (@PathVariable String id){
+        return rickAndMortyClient.findAndCharacterById(id);
+    }
+}

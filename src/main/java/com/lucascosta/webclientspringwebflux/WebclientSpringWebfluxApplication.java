d689db7eca61fbdb84b72cfa98845fd320cee2b2
0xaf0y <lucas.santoscosta@gmail.com>
2024-01-14 09:04:06 -0300

    Commit inicial

---
+++ src/main/java/com/lucascosta/webclientspringwebflux/WebclientSpringWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.lucascosta.webclientspringwebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebclientSpringWebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebclientSpringWebfluxApplication.class, args);
+	}
+
+}
